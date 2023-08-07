dd68a2332df3826e046b1122357e6e16901c8d65
Gael Estrera <gaelestrera@gmail.com>
2023-08-08 02:56:00 +0800

    Data Structures in front-end for editing Associative Entity

--- src/pages/api/achievements/[id]/edit.tsx
+++ src/pages/api/achievements/[id]/edit.tsx
@@ -2,7 +2,7 @@ import connection from "@/api/database";
 
 export default function Edit(req: any, res: any){
     const {method, body, query} = req;
-    const {title, Description, Duration, AchID, skillsID, skills} = body;
+    const {title, Description, Duration, AchID, skills} = body;
     const id = query.id;
 
     if (method === 'PUT'){
@@ -17,20 +17,22 @@ export default function Edit(req: any, res: any){
         connection.query(q1, v1, (error, result) => {
             if(error) return console.error(error);
             let i = 0;
-            //while (i < )
-            for (let skillName of skills){
-                let v2 = [skillName, id]
+            while (i < skills.length){
+                let v2 = [skills[i][0], skills[i][0], skills[i][0], id, skills[i][1]]
                 let q2 = 
                 `
                     UPDATE AchSkills, Skills
-                    SET AchSkills.sID = Skills.id
-                    WHERE Skills.name = ?
-                    AND Skills.I
+                    SET AchSkills.sID = ?
+                    WHERE Skills.id = ?
+                    AND AchSkills.sID = ?
+                    AND AchSkils.aID = ? 
+                    AND Skills.name = ?
                 `
                 connection.query(q2, v2, (error, result) => {
                     if (error) return console.error(error);
                 })
             }
+            
             return res.status(200).json({result: 'success'})
 
         })
